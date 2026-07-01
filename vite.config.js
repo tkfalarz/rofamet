40e84d834b1ebd424054fc6b2be04598cbcdde39
Tomasz Falarz <t.k.falarz@gmail.com>
2026-07-01 21:55:50 +0200

    enj

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/rofamet/',
   plugins: [react()],
   build: {
     target: 'es2020',
